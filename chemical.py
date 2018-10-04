b2c749d956c091b264de2918821523f77133a58b
Unknown <a77364@alunos.uminho.pt>
2018-10-04 16:52:57 +0100

    Opções de input e output, remoção de acentos e versão inicial dos extra

--- chemical.py
+++ chemical.py
@@ -1,16 +1,66 @@
+#!/usr/bin/python3
+
 import re
 import regex
+import sys
+import getopt
 
-chemical_symbols = ['Ac', 'Ag', 'Al', 'Am', 'Ar', 'As', 'At', 'Au', 'Ba', 'Bo', 'Be', 'Bh', 'Bi', 'Bk', 'Br', 'Ca', 'Cd', 'C', 'Ce', 'Cf', 'Cl', 'Cn', 'Cm', 'Co', 'Cr', 'Cs', 'Cu', 'Ds', 'Db', 'Dy', 'Er', 'Es', 'Eu', 'Fm', 'Fl', 'F', 'Fe', 'Fr', 'Ga', 'Gd', 'Ge', 'H', 'He', 'Hg', 'Hf', 'Ho', 'Hs', 'I', 'In', 'Ir', 'K', 'Kr', 'La', 'Li', 'Lr', 'Lv', 'Lu', 'Md', 'Mg', 'Mn', 'Mt', 'Mo', 'Mc', 'N', 'Na', 'Nb', 'Nd', 'Ne', 'Ni', 'Nh', 'No', 'Np', 'O', 'Og', 'Os', 'Pb', 'P', 'Pa', 'Pd', 'Po', 'Pr', 'Pm', 'Pt', 'Pu', 'Ra', 'Rb', 'Re', 'Rf', 'Rg', 'Rh', 'Rn', 'Ru', 'S', 'Sb', 'Sc', 'Sm', 'Sg', 'Se', 'Si', 'Sn', 'Sr', 'Ta', 'Tb', 'Tc', 'Te', 'Th', 'Ts', 'Tl', 'Ti', 'Tm', 'W', 'U', 'V', 'Xe', 'Y', 'Yb', 'Zn', 'Zr']
+def clean_accents(word):
+    word = re.sub(r'[áàãâ]', r'a', word)
+    word = re.sub(r'[ÁÀÃÂ]', r'A', word)
+    word = re.sub(r'[éèê]', r'e', word)
+    word = re.sub(r'[ÉÈÊ]', r'E', word)
+    word = re.sub(r'[íìî]', r'i', word)
+    word = re.sub(r'[ÍÌÎ]', r'I', word)
+    word = re.sub(r'[óòõô]', r'o', word)
+    word = re.sub(r'[ÓÒÕÔ]', r'O', word)
+    word = re.sub(r'[úùû]', r'u', word)
+    word = re.sub(r'[ÚÙÛ]', r'U', word)
+    return word
 
-pattern = r'\t(' + '|'.join(chemical_symbols) + ')+$'
+def main ():
 
-f = open('palavras.txt', 'r')
+    inputfile = ''
+    outputfile = ''
+    try: 
+        opts, args = getopt.getopt(sys.argv[1:],"i:o:hv",["ifile=","ofile=","help=","version="])
+    except getopt.GetoptError:
+        print('chemical [-i <inputfile>] [-o <outputfile>]')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('chemical [-i <inputfile>] [-o <outputfile>]')
+            sys.exit()
+        elif opt == '-v':
+            print('Version 1.0')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
 
-for line in f:
-    if re.search(pattern, line, re.IGNORECASE):
-        new_line = line.rstrip().split('\t')
-        composition = regex.match(r'^(' + '|'.join(chemical_symbols) + ')+$', new_line[-1], flags=regex.IGNORECASE).captures(1)
-        print(new_line[-1] + ": " + " + ".join(composition))
+    chemical_symbols = ['Ac', 'Ag', 'Al', 'Am', 'Ar', 'As', 'At', 'Au', 'Ba', 'Bo', 'Be', 'Bh', 'Bi', 'Bk', 'Br', 'Ca', 'Cd', 'C', 'Ce', 'Cf', 'Cl', 'Cn', 'Cm', 'Co', 'Cr', 'Cs', 'Cu', 'Ds', 'Db', 'Dy', 'Er', 'Es', 'Eu', 'Fm', 'Fl', 'F', 'Fe', 'Fr', 'Ga', 'Gd', 'Ge', 'H', 'He', 'Hg', 'Hf', 'Ho', 'Hs', 'I', 'In', 'Ir', 'K', 'Kr', 'La', 'Li', 'Lr', 'Lv', 'Lu', 'Md', 'Mg', 'Mn', 'Mt', 'Mo', 'Mc', 'N', 'Na', 'Nb', 'Nd', 'Ne', 'Ni', 'Nh', 'No', 'Np', 'O', 'Og', 'Os', 'Pb', 'P', 'Pa', 'Pd', 'Po', 'Pr', 'Pm', 'Pt', 'Pu', 'Ra', 'Rb', 'Re', 'Rf', 'Rg', 'Rh', 'Rn', 'Ru', 'S', 'Sb', 'Sc', 'Sm', 'Sg', 'Se', 'Si', 'Sn', 'Sr', 'Ta', 'Tb', 'Tc', 'Te', 'Th', 'Ts', 'Tl', 'Ti', 'Tm', 'W', 'U', 'V', 'Xe', 'Y', 'Yb', 'Zn', 'Zr']
 
-f.close()
+    pattern = r'^(' + '|'.join(chemical_symbols) + ')+$'
+
+    if not inputfile:
+        fin = sys.stdin
+    else:
+        fin = open(inputfile, 'r')
+
+    if not outputfile:
+        fout = sys.stdout
+    else:
+        fout = open(outputfile, 'w')
+
+    for line in fin:
+        word = line.rstrip().split('\t')[-1]            ########## TODO: Corrigir depois de tirar dúvida
+        word_no_acc = clean_accents(word)
+        if re.search(pattern, word_no_acc, re.IGNORECASE):
+            composition = regex.match(r'^(' + '|'.join(chemical_symbols) + ')+$', word_no_acc, flags=regex.IGNORECASE).captures(1)
+            fout.write(word + ": " + " + ".join(composition) + "\n")
+
+    fin.close()
+    fout.close()
+
+main()
--- chemical2.py
+++ chemical2.py
@@ -1,16 +1,82 @@
+#!/usr/bin/python3
+
 import re
+import regex
+import sys
+import getopt
+import pubchempy as pcp
 
-pattern = r'^(Ac|Ag|Al|Am|Ar|As|At|Au|Ba|Bo|Be|Bh|Bi|Bk|Br|Ca|Cd|C|Ce|Cf|Cl|Cn|Cm|Co|Cr|Cs|Cu|Ds|Db|Dy|Er|Es|Eu|Fm|Fl|F|Fe|Fr|Ga|Gd|Ge|H|He|Hg|Hf|Ho|Hs|I|In|Ir|K|Kr|La|Li|Lr|Lv|Lu|Md|Mg|Mn|Mt|Mo|Mc|N|Na|Nb|Nd|Ne|Ni|Nh|No|Np|O|Og|Os|Pb|P|Pa|Pd|Po|Pr|Pm|Pt|Pu|Ra|Rb|Re|Rf|Rg|Rh|Rn|Ru|S|Sb|Sc|Sm|Sg|Se|Si|Sn|Sr|Ta|Tb|Tc|Te|Th|Ts|Tl|Ti|Tm|W|U|V|Xe|Y|Yb|Zn|Zr)+$'
 
-# pattern = r'^(Ba|Co|N|Ta)+$'
+def clean_accents(word):
+    word = re.sub(r'[áàãâ]', r'a', word)
+    word = re.sub(r'[ÁÀÃÂ]', r'A', word)
+    word = re.sub(r'[éèê]', r'e', word)
+    word = re.sub(r'[ÉÈÊ]', r'E', word)
+    word = re.sub(r'[íìî]', r'i', word)
+    word = re.sub(r'[ÍÌÎ]', r'I', word)
+    word = re.sub(r'[óòõô]', r'o', word)
+    word = re.sub(r'[ÓÒÕÔ]', r'O', word)
+    word = re.sub(r'[úùû]', r'u', word)
+    word = re.sub(r'[ÚÙÛ]', r'U', word)
+    return word
 
-f = open('teste.txt', 'r')
 
-lineno = 0
+def main ():
 
-for line in f:
-    lineno += 1
-    if re.search(pattern, line, re.IGNORECASE):
-        print(lineno, line, sep=': ', end='')
+    inputfile = ''
+    outputfile = ''
+    try: 
+        opts, args = getopt.getopt(sys.argv[1:],"i:o:hv",["ifile=","ofile=","help=","version="])
+    except getopt.GetoptError:
+        print('chemical [-i <inputfile>] [-o <outputfile>]')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('chemical [-i <inputfile>] [-o <outputfile>]')
+            sys.exit()
+        elif opt == '-v':
+            print('Version 1.0')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
 
-f.close()
+    chemical_symbols = ['Ac', 'Ag', 'Al', 'Am', 'Ar', 'As', 'At', 'Au', 'Ba', 'Bo', 'Be', 'Bh', 'Bi', 'Bk', 'Br', 'Ca', 'Cd', 'C', 'Ce', 'Cf', 'Cl', 'Cn', 'Cm', 'Co', 'Cr', 'Cs', 'Cu', 'Ds', 'Db', 'Dy', 'Er', 'Es', 'Eu', 'Fm', 'Fl', 'F', 'Fe', 'Fr', 'Ga', 'Gd', 'Ge', 'H', 'He', 'Hg', 'Hf', 'Ho', 'Hs', 'I', 'In', 'Ir', 'K', 'Kr', 'La', 'Li', 'Lr', 'Lv', 'Lu', 'Md', 'Mg', 'Mn', 'Mt', 'Mo', 'Mc', 'N', 'Na', 'Nb', 'Nd', 'Ne', 'Ni', 'Nh', 'No', 'Np', 'O', 'Og', 'Os', 'Pb', 'P', 'Pa', 'Pd', 'Po', 'Pr', 'Pm', 'Pt', 'Pu', 'Ra', 'Rb', 'Re', 'Rf', 'Rg', 'Rh', 'Rn', 'Ru', 'S', 'Sb', 'Sc', 'Sm', 'Sg', 'Se', 'Si', 'Sn', 'Sr', 'Ta', 'Tb', 'Tc', 'Te', 'Th', 'Ts', 'Tl', 'Ti', 'Tm', 'W', 'U', 'V', 'Xe', 'Y', 'Yb', 'Zn', 'Zr']
+
+    pattern = r'^(' + '|'.join(chemical_symbols) + ')+$'
+
+    if not inputfile:
+        fin = sys.stdin
+    else:
+        fin = open(inputfile, 'r')
+
+    if not outputfile:
+        fout = sys.stdout
+    else:
+        fout = open(outputfile, 'w')
+
+    fout.write("\\documentclass{article}\n\n\\begin{document}\n\n")
+
+    content = re.findall(r'\w+|\W+', fin.read())
+
+    for word in content:
+        word_no_acc = clean_accents(word)
+        if re.search(pattern, word_no_acc, re.IGNORECASE):
+            composition = regex.match(r'^(' + '|'.join(chemical_symbols) + ')+$', word_no_acc, flags=regex.IGNORECASE).captures(1)
+            try:
+                for cid in pcp.get_cids(word, 'name'):
+                    info = pcp.Compound.from_cid(cid).iupac_name
+                fout.write(" + ".join(composition)  + ' (' + info + ')')
+            except pcp.PubChemHTTPError:
+                info = ''
+        else:
+            fout.write(word.replace("_","\_").replace("\n","\n\n"))
+        print(word)
+
+    fout.write("\n\\end{document}\n")
+
+    fin.close()
+    fout.close()
+
+main()
