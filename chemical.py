dcdcff56be108cd6a96f779a38b8b0e1cd0bc533
João Pires Barreira <jpiresbarreira@gmail.com>
2018-10-04 13:31:11 +0100

    Adicionados ficheiros do problema 2 do tp1

---
+++ chemical.py
@@ -0,0 +1,17 @@
+import re
+
+pattern = r'\t(Ac|Ag|Al|Am|Ar|As|At|Au|Ba|Bo|Be|Bh|Bi|Bk|Br|Ca|Cd|C|Ce|Cf|Cl|Cn|Cm|Co|Cr|Cs|Cu|Ds|Db|Dy|Er|Es|Eu|Fm|Fl|F|Fe|Fr|Ga|Gd|Ge|H|He|Hg|Hf|Ho|Hs|I|In|Ir|K|Kr|La|Li|Lr|Lv|Lu|Md|Mg|Mn|Mt|Mo|Mc|N|Na|Nb|Nd|Ne|Ni|Nh|No|Np|O|Og|Os|Pb|P|Pa|Pd|Po|Pr|Pm|Pt|Pu|Ra|Rb|Re|Rf|Rg|Rh|Rn|Ru|S|Sb|Sc|Sm|Sg|Se|Si|Sn|Sr|Ta|Tb|Tc|Te|Th|Ts|Tl|Ti|Tm|W|U|V|Xe|Y|Yb|Zn|Zr)+$'
+
+# pattern = r'^(Ba|Co|N|Ta)+$'
+
+f = open('palavras.txt', 'r')
+
+lineno = 0
+
+for line in f:
+    lineno += 1
+    if re.search(pattern, line, re.IGNORECASE):
+        new_line = line.split('\t')
+        print(lineno, new_line[-1], sep=': ', end='')
+
+f.close()
