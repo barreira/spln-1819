9b19d9bc06471b9162ead9375445ef620faf210f
João Pires Barreira <jpiresbarreira@gmail.com>
2018-11-22 11:09:34 +0000

    Adicionados en e pt

--- tp2/app.py
+++ tp2/app.py
@@ -4,16 +4,16 @@ from pos_tagging import generate_tagged_text, generate_pos_information, generate
 
 app = Flask(__name__)
 
-nlp = spacy.load('pt')
-# spacy.load('en_core_web_lg')
 tagged_text = ''
 pos_info = ''
 graphs = ''
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global nlp, tagged_text, pos_info, graphs 
     if request.method == 'POST':
+        nlp = spacy.load(request.values.get('lang'))
         doc = nlp(request.values.get('input'))
         tagged_text = generate_tagged_text(doc, type = 'html')
         pos_info = generate_pos_information(doc)
@@ -21,13 +21,15 @@ def index():
         return redirect(url_for('tagged_text_form'))
     return render_template('index.html')
 
+
 @app.route('/tagged_text_form', methods=['GET', 'POST'])
 def tagged_text_form():
     global tagged_text
     if request.method == 'POST':
         return redirect(url_for('index'))
     return render_template('tagged_text_form.html', tagged_text=tagged_text)
-    
+
+
 @app.route('/pos_info_form', methods=['GET', 'POST'])
 def pos_info_form():
     global pos_info
@@ -35,12 +37,14 @@ def pos_info_form():
         return redirect(url_for('index'))
     return render_template('pos_info_form.html', table=pos_info)
 
+
 @app.route('/graphs_form', methods=['GET', 'POST'])
 def graphs_form():
     global graphs
     if request.method == 'POST':
         return redirect(url_for('index'))
     return render_template('graphs_form.html', graphs=graphs)
+
 
 if __name__ == '__main__':
     app.run()
