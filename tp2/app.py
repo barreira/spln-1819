838a04ed6abe0173af87bb2b049069c08eae2a75
Unknown <a77364@alunos.uminho.pt>
2018-11-26 20:59:16 +0000

    [TP2] Corrigido tooltip no index.html e adição de opções na linha de comandos

--- tp2/app.py
+++ tp2/app.py
@@ -1,19 +1,65 @@
-from flask import Flask, request, url_for, redirect, render_template
+import sys
+import getopt
+import json
 import spacy
 from pos_tagging import *
-import json
+from flask import Flask, request, url_for, redirect, render_template
+
+# Processar argumentos do comando
+def processArgs():
+    info_out = ''
+    tagged_text_out = ''
+    graphs_out = ''
+    lang = 'pt'
+    inputfile = ''
+
+    error = 'USAGE:\tpython3 app.py [(-i <outputfile> | -t <outputfile> | -g <outputfile>) [-l <language>] <inputfile>]\n\n' + \
+            'OPTIONS:\n' + \
+            '\t-l\tlanguage - \'pt\' (default) or \'en\'\n' + \
+            '\t-i\treturns table with information about tokens to outputfile (or \'shell\')\n' + \
+            '\t-t\treturns tagged text to outputfile (or \'shell\')\n' + \
+            '\t-g\treturns dependencies graphs to outputfile.svg\n' +\
+            '* if no options are provided, a web server will be initialized, where the input will be inserted and the output presented.'
+
+    # Processar opções/argumentos do comando utilizado
+    try: 
+        opts, args = getopt.getopt(sys.argv[1:],"i:t:g:l:hv",["info=","tagged-text=", "graphs=", "lang=","help","version"])
+    except getopt.GetoptError:
+        print(error)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print(error)
+            sys.exit()
+        elif opt in ('-v', '--version'):
+            print('Version 1.0')
+            sys.exit()
+        elif opt in ("-i", "--info"):
+            info_out = arg
+        elif opt in ("-t", "--tagged-text"):
+            tagged_text_out = arg
+        elif opt in ("-g", "--graphs"):
+            graphs_out = arg
+        elif opt in ("-l", "--lang"):
+            lang = arg
+    
+    if info_out or tagged_text_out or graphs_out:
+        if len(args) == 1:
+            inputfile = args[0]
+        else:
+            print(error)
+            sys.exit(2)
+    elif len(args)>0:
+        print(error)
+        sys.exit(2)
+
+    return info_out, tagged_text_out, graphs_out, lang, inputfile
 
 app = Flask(__name__)
 
-tagged_text = ''
-pos_info = ''
-graphs = ''
-nlp = {'en': spacy.load('en_core_web_lg'), 'pt': spacy.load('pt') }
-lang = 'pt'
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global lang, nlp, tagged_text, pos_info, graphs
+    global lang, nlp, tagged_text, info, graphs
     if request.method == 'POST':
         lang = request.values.get('lang')
         entities = request.form.getlist('entity')
@@ -21,7 +67,7 @@ def index():
         add_tokenizer_exceptions(nlp[lang], token_exceptions)
         doc = nlp[lang](request.values.get('input'))
         tagged_text = generate_tagged_text(doc, type = 'html', entities = entities)
-        pos_info = generate_pos_information(doc, nlp[lang].vocab)
+        info = generate_information(doc, nlp[lang].vocab)
         graphs = '\n'.join(generate_dependencies_graph(doc, type = 'html'))
         return redirect(url_for('tagged_text_form'))
     return render_template('index.html', lang=lang)
@@ -35,12 +81,12 @@ def tagged_text_form():
     return render_template('tagged_text_form.html', tagged_text=tagged_text)
 
 
-@app.route('/pos_info_form', methods=['GET', 'POST'])
-def pos_info_form():
-    global pos_info
+@app.route('/info_form', methods=['GET', 'POST'])
+def info_form():
+    global info
     if request.method == 'POST':
         return redirect(url_for('index'))
-    return render_template('pos_info_form.html', table=pos_info)
+    return render_template('info_form.html', table=info)
 
 
 @app.route('/graphs_form', methods=['GET', 'POST'])
@@ -52,4 +98,40 @@ def graphs_form():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    global tagged_text, info, graphs
+    info_out, tagged_text_out, graphs_out, lang, inputfile = processArgs()
+    if inputfile:
+        if lang == 'en':
+            nlp = spacy.load('en_core_web_lg')
+        else:
+            nlp = spacy.load('pt')
+        with open(inputfile, 'r') as input:
+            doc = nlp(input.read())
+        if info_out:
+            output = generate_information(doc, nlp.vocab, 'text')
+            if info_out == 'shell':
+                print(output)
+            else:
+                with open(info_out, 'w') as fd:
+                    fd.write(output)
+        if tagged_text_out:
+            output = generate_tagged_text(doc, 'text', nlp.vocab)
+            if tagged_text_out == 'shell':
+                print(output)
+            else:
+                with open(tagged_text_out, 'w') as fd:
+                    fd.write(output)
+        if graphs_out:
+            os.makedirs('./images', exist_ok=True)
+            pictures = generate_dependencies_graph(doc, 'pict')
+            for i, pict in enumerate(pictures):
+                file_name = graphs_out + '_' + str(i) + '.svg'
+                output_path = Path('./images/' + file_name)
+                with output_path.open('w', encoding='utf-8') as output_file:
+                    output_file.write(pict)
+    else:
+        nlp = {'en': spacy.load('en_core_web_lg'), 'pt': spacy.load('pt') }
+        tagged_text = ''
+        info = ''
+        graphs = ''
+        app.run()
--- tp2/pos_tagging.py
+++ tp2/pos_tagging.py
@@ -4,6 +4,7 @@ import spacy
 from spacy import displacy
 from spacy.util import update_exc
 from pathlib import Path
+from prettytable import PrettyTable
 
 # https://spacy.io/usage/linguistic-features
 
@@ -17,8 +18,15 @@ def generate_html_table(headers, data):
     res += '  </tbody>\n</table>\n'
     return res
 
+def generate_table(headers, data):
+    table = PrettyTable()
+    table.field_names = headers
+    for row in data:
+        table.add_row(row)
+    return table
+
 # Part-of-speech tagging
-def generate_pos_information(doc, vocab):
+def generate_information(doc, vocab, type='html'):
     # python3 -m spacy download pt
     headers = ["Text","Lemma", "POS", "TAG", "DEP", "SHAPE", "MORPHOLOGIAL INFO", "IS_ALPHA", "IS_STOP"]
     data = []
@@ -26,21 +34,27 @@ def generate_pos_information(doc, vocab):
 
     for token in doc:
         if (str(token.pos_) != 'SPACE' and str(token.pos_) != 'PUNCT') or token.text not in tokens:
-            morph_info = dict(filter(lambda x : x[0]!=74, vocab.morphology.tag_map[token.tag_].items()))
-            if not morph_info:
+            if token.tag_:
+                morph_info = dict(filter(lambda x : x[0]!=74, vocab.morphology.tag_map[token.tag_].items()))
+                if not morph_info:
+                    morph_info = ''
+            else:
                 morph_info = ''
             data.append([str(s) for s in (token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
                     token.shape_, morph_info, token.is_alpha, token.is_stop)])
             tokens.append(token.text)
-    
-    return generate_html_table(headers, data)
+
+    if type=='html':
+        return generate_html_table(headers, data)
+    else:
+        return generate_table(headers, data)
 
 
 # Dependencies Graph
 def generate_dependencies_graph(doc, type='service', compact = False, background='white', 
                                             color='black', font='Source Sans Pro'):
     res = []
-    if type in ('service', 'html', 'svg'):
+    if type in ('service', 'html', 'pict'):
         docs = list(doc.sents)
         options = {'compact': compact, 'bg': background, 'color': color, 'font': font}
     if type == 'service':
@@ -49,10 +63,10 @@ def generate_dependencies_graph(doc, type='service', compact = False, background
         html = displacy.render(docs, style='dep', page=True, options=options)
         html = re.sub(r'.*<body[^>]*>(.*)</body>.*', r'\1', html, flags=re.DOTALL)
         res.append(html)
-    elif type == 'svg':
+    elif type == 'pict':
         for doc in docs:
-            svg = displacy.render(doc, style='dep', options=options)
-            res.append(svg)
+            pict = displacy.render(doc, style='dep', options=options)
+            res.append(pict)
     return res
 
 
@@ -69,6 +83,14 @@ def generate_tagged_text(doc, type = 'server', entities = None, colors = None):
             displacy.serve(doc, style='ent', options=options)
         else:
             res = displacy.render(doc, style='ent', options=options)
+    else:
+        res_list = []
+        for word in doc:
+            if word.ent_type_:
+                res_list.append(str(word) + '{' + word.ent_type_ + '}')
+            else:
+                res_list.append(str(word))
+        res = ' '.join(res_list)
     return res
 
 # Rule-based morphology
