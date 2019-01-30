4336e363854f99ef6c964a4dc95fc8dbbf861818
Unknown <a77364@alunos.uminho.pt>
2019-01-30 22:33:04 +0000

    [TP3] Organização ficheiros e obtenção de medidas médias para várias operações

--- tp3/scripts/metrics_measure.py
+++ tp3/scripts/metrics_measure.py
@@ -4,7 +4,8 @@ import regex
 import string
 import fileinput
 import numpy as np
-import spellcorrector_tags
+import spellcorrector
+from prettytable import PrettyTable
 
 def is_word(token):
     if regex.search(r'\w+', token, flags=regex.UNICODE):
@@ -76,32 +77,39 @@ def classification(original_word, errors_word, corrected_word):
         return None
 
 
-def main():
-    # Ler texto correto
-    print('-- Leitura do texto correto --', file=sys.stderr)
-    original_text = ''.join([line for line in fileinput.input(sys.argv[1:])])
-    original_tokens = regex.findall(r'\w+|\s+|\p{P}+', original_text, flags=regex.UNICODE)
+def get_wrong_texts(n_tests, original_tokens):
+    wrong_texts = []
+    with open('aux_files/errors.txt', 'w+') as fd:
+        for i in range(n_tests):
+            errors_text = gen_text_with_errors(original_tokens)
+            wrong_texts.append(errors_text)
+            print(f'\n-- TESTE {i} --', file=fd)
+            print(errors_text, file=fd)
+    return wrong_texts
 
-    # Gerar texto com erros
-    print('-- Inserção de erros no texto --', file=sys.stderr)
-    errors_text = gen_text_with_errors(original_tokens)
+
+def get_metrics(function_name, id_test, function, original_tokens, errors_text):
     errors_tokens = regex.findall(r'\w+|\s+|\p{P}+', errors_text, flags=regex.UNICODE)
-    with open('errors.txt', 'w+') as fd:
-        print(errors_text, file=fd)
 
     # Corrigir erros do texto
-    print('-- Correção dos erros do texto --', file=sys.stderr)
-    t1 = time.time()
-    pos_freq, words_freq = spellcorrector_tags.analyze_large_text()
-    t2 = time.time()
-    corrected_text = spellcorrector_tags.correct_text(pos_freq, words_freq, text_lines=errors_text.split('\n'))
-    t3 = time.time()
-    corrected_tokens = regex.findall(r'\w+|\s+|\p{P}+', corrected_text, flags=regex.UNICODE)
-    with open('corrected.txt', 'w+') as fd:
-        print(corrected_text, file=fd)
+    print(f'-- Correção dos erros do texto com função {function_name} - Teste {str(id_test)} --', file=sys.stderr)
+    if function_name == 'Manual':
+        t1 = time.time()
+        pos_freq, words_freq = spellcorrector.analyze_large_text()
+        t2 = time.time()
+        corrected_text = spellcorrector.correct_text(pos_freq, words_freq, text_lines=errors_text.split('\n'))
+        t3 = time.time()
+        duration = (t2-t1, t3-t2)
+    else:
+        t1 = time.time()
+        corrected_text = spellcorrector.correct_text_ext(errors_text.split('\n'), function)
+        t2 = time.time()
+        duration = t2-t1
 
-    print('Load Time:\t\t', round(t2-t1,2), 'sec')
-    print('Correction Time:\t', round(t3-t2,2), 'sec\n')
+    corrected_tokens = regex.findall(r'\w+|\s+|\p{P}+', corrected_text, flags=regex.UNICODE)
+    with open('aux_files/corrected.txt', 'a+') as fd:
+        print(f'\n-- FUNÇÃO {function_name} - TESTE {id_test} --', file=fd)
+        print(corrected_text, file=fd)
 
     # Calcular métricas
     # TODO: DUVIDA NAS METRICAS
@@ -133,41 +141,159 @@ def main():
             elif c == 'FN':
                 false_negatives += 1
     
+    return true_positives_wrong_correction, true_positives_right_correction, true_negatives, false_positives, false_negatives, total_words, duration
+
+
+def run_tests(function_name, function, original_tokens, wrong_texts, n_tests):
+    true_positives_wrong_correction = 0  # Há erro e token é corrigido incorretamente
+    true_positives_right_correction = 0  # Há erro e token é corrigido corretamente
+    true_negatives = 0  # Não há erro e token mantém-se
+    false_positives = 0 # Não há erro, mas token é corrigido
+    false_negatives = 0 # Há erro, mas este não é corrigido
+    total_words = 0
+    duration = 0
+
+    for i in range(0,n_tests):
+        tpw, tpr, tn, fp, fn, tw, d = get_metrics(function_name, i, function, original_tokens, wrong_texts[i])
+        true_positives_wrong_correction += tpw
+        true_positives_right_correction += tpr
+        true_negatives += tn
+        false_positives += fp
+        false_negatives += fn
+        total_words += tw
+        if isinstance(d, tuple):
+            if i == 0:
+                duration = [d[0],d[1]]
+            else:
+                duration[0] += d[0]
+                duration[1] += d[1]
+        else:
+            duration += d
+    
+    true_positives_wrong_correction /= n_tests
+    true_positives_right_correction /= n_tests
+    true_negatives /= n_tests
+    false_positives /= n_tests
+    false_negatives /= n_tests
+    total_words /= n_tests
+    if isinstance(duration, list):
+        duration[0] /= n_tests
+        duration[1] /= n_tests
+    else:
+        duration /= n_tests
+
     true_positives = true_positives_right_correction + true_positives_wrong_correction
     
-    print('True Positives Wrong:\t', true_positives_wrong_correction)
-    print('True Positives Right:\t', true_positives_right_correction)
-    print('True Negatives:\t\t', true_negatives)
-    print('False Positives:\t', false_positives)
-    print('False Negatives:\t', false_negatives)
-    
-    print()
-
     if total_words > 0:
-        accuracy = (true_positives_right_correction + true_negatives) / total_words
-        print('Accuracy:\t', round(accuracy,2))
+        accuracy = round( (true_positives_right_correction + true_negatives) / total_words, 2 )
 
         if true_positives + false_positives > 0:
-            precision = true_positives_right_correction / (true_positives + false_positives)
-            print('Precision:\t', round(precision,2))
+            precision = round( true_positives_right_correction / (true_positives + false_positives) , 2 )
+        else:
+            precision = None
         
         if true_positives + false_negatives > 0:
-            recall = true_positives_right_correction / (true_positives + false_negatives)
-            print('Recall:\t\t', round(recall,2))
+            recall = round( true_positives_right_correction / (true_positives + false_negatives) , 2 )
+        else:
+            recall = None
         
-        if precision and recall and precision + recall>0:
-            harmonic_mean = 2 * ((precision * recall) / (precision + recall))
-            print('Harmonic Mean:\t', round(harmonic_mean,2))
-        
+        if precision and recall and precision + recall > 0:
+            harmonic_mean = round( 2 * ((precision * recall) / (precision + recall)) , 2 )
+        else:
+            harmonic_mean = None
+                
         if true_positives + false_negatives > 0:
-            true_positive_rate = true_positives_right_correction / (true_positives + false_negatives)
-            print('TPR:\t\t', round(true_positive_rate,2))
-
+            true_positive_rate = round( true_positives_right_correction / (true_positives + false_negatives) , 2 )
+        else:
+            true_positive_rate = None
+        
         if false_positives + true_negatives > 0:
-            false_positive_rate = false_positives / (false_positives + true_negatives)
-            print('FPR:\t\t', round(false_positive_rate,2))
+            false_positive_rate = round( false_positives / (false_positives + true_negatives) , 2 )
+        else:
+            false_positive_rate = None
+    else:
+        accuracy = None
+        precision = None
+        recall = None
+        harmonic_mean = None
+        true_positive_rate = None
+        false_positive_rate = None
 
-    # TODO: Gerar ROC e medir AUC
+    return true_positives_wrong_correction, true_positives_wrong_correction, true_negatives,\
+            false_positives, false_negatives, accuracy, precision, recall, harmonic_mean,\
+            true_positive_rate, false_positive_rate, duration
+
 
 if __name__ == "__main__":
-    main()
+    
+    # Ler texto correto
+    print('-- Leitura do texto correto --', file=sys.stderr)
+    original_text = ''.join([line for line in fileinput.input(sys.argv[1:])])
+    original_tokens = regex.findall(r'\w+|\s+|\p{P}+', original_text, flags=regex.UNICODE)
+    
+    print('-- Inserção de erros no texto original (5x) --', file=sys.stderr)
+    wrong_texts = get_wrong_texts(5, original_tokens)
+
+    # Gerar estrutura da tabela output
+    table = PrettyTable()
+    table.field_names = ["Medidas", "Manual", "Aspell", "Hunspell", "Symspelly"]
+    tpw_list = ['True Positives Wrong']
+    tpr_list = ['True Positives Right']
+    tn_list = ['True Negatives']
+    fp_list = ['False Positives']
+    fn_list = ['False Negatives']
+    accuracy_list = ['Accuracy']
+    precision_list = ['Precision']
+    recall_list = ['Recall']
+    hm_list = ['Harmonic Mean']
+    tp_rate_list = ['TP Rate']
+    fp_rate_list = ['FP Rate']
+    d_list = ['Duration (sec)']
+
+    tpw, tpr, tn, fp, fn, accuracy, precision, recall, hm, tp_rate, fp_rate, d = \
+        run_tests('Manual', spellcorrector.correct_text, original_tokens, wrong_texts, 5)
+    tpw_list.append(tpw)
+    tpr_list.append(tpr)
+    tn_list.append(tn)
+    fp_list.append(fp)
+    fn_list.append(fn)
+    accuracy_list.append(accuracy)
+    precision_list.append(precision)
+    recall_list.append(recall)
+    hm_list.append(hm)
+    tp_rate_list.append(tp_rate)
+    fp_rate_list.append(fp_rate)
+    d_list.append(f'{round(d[0],2)} + {round(d[1],2)}')
+
+    function_names = ["Aspell", "Hunspell", "Symspelly"]
+    functions = [spellcorrector.correct_line_aspell, spellcorrector.correct_line_hunspell, spellcorrector.correct_line_symspelly]
+    for i in range(0,3):
+        tpw, tpr, tn, fp, fn, accuracy, precision, recall, hm, tp_rate, fp_rate, d = \
+            run_tests(function_names[i], functions[i], original_tokens, wrong_texts, 5)
+        tpw_list.append(tpw)
+        tpr_list.append(tpr)
+        tn_list.append(tn)
+        fp_list.append(fp)
+        fn_list.append(fn)
+        accuracy_list.append(accuracy)
+        precision_list.append(precision)
+        recall_list.append(recall)
+        hm_list.append(hm)
+        tp_rate_list.append(tp_rate)
+        fp_rate_list.append(fp_rate)
+        d_list.append(str(round(d,2)))
+
+    table.add_row(tpw_list)
+    table.add_row(tpr_list)
+    table.add_row(tn_list)
+    table.add_row(fp_list)
+    table.add_row(fn_list)
+    table.add_row(accuracy_list)
+    table.add_row(precision_list)
+    table.add_row(recall_list)
+    table.add_row(hm_list)
+    table.add_row(tp_rate_list)
+    table.add_row(fp_rate_list)
+    table.add_row(d_list)
+
+    print(table)
--- tp3/scripts/spellcorrector.py
+++ tp3/scripts/spellcorrector.py
@@ -1,4 +1,4 @@
-import re
+import regex
 import os
 import sys
 import pickle
@@ -14,26 +14,64 @@ import hunspell
 from collections import Counter, defaultdict
 from symspellpy.symspellpy import SymSpell, Verbosity
 
-#################### VERSÃO COM EXPRESSÕES REGULARES ####################
 
-def words(text): return re.findall(r'\w+', text.lower())
+def get_tag(token):
+    tags = list(filter(lambda x: x[0]!='<' and x[-1]!='>' and x[0]!='@', token.tag_.split('|')))
+    if token.pos_ == 'ADV':
+        t = str(token)
+        if t == 'agora' or t == 'hoje':
+            tags += '|Present'
+        elif t == 'anteontem' or t == 'antes' or t == 'ontem' or t == 'outrora':
+            tags += '|Past'
+        elif t == 'amanhã' or t == 'breve':
+            tags += '|Future'
+    return '<' + '|'.join(tags) + '>'
+
+#################### VERSÃO MANUAL ####################
+
+def words(text): return regex.findall(r'\w+', text.lower())
 
 # WORDS = Counter(words(open('dictionaries/wordlist-ao-latest.txt').read()))
 
-def P(word, words_freq, pos_freqs, N_pos, context_pos): 
+def get_pos_context(before_word, word, after_word, words_freq, nlp):
+    if before_word and after_word:
+        doc = nlp(before_word + ' ' + word + ' ' + after_word)
+        pos_0 = '<UNKNOWN>' if before_word not in words_freq else doc[0].pos_ + get_tag(doc[0])
+        pos_1 = '<UNKNOWN>' if word not in words_freq else doc[1].pos_ + get_tag(doc[1])
+        pos_2 = '<UNKNOWN>' if after_word not in words_freq else doc[2].pos_ + get_tag(doc[2])
+        context_pos = pos_0 + ' ' + pos_1 + ' ' + pos_2
+    elif not before_word and after_word:
+        doc = nlp(word + ' ' + after_word)
+        pos_1 = '<UNKNOWN>' if word not in words_freq else doc[0].pos_ + get_tag(doc[0])
+        pos_2 = '<UNKNOWN>' if after_word not in words_freq else doc[1].pos_ + get_tag(doc[1])
+        context_pos = '__BEGIN__ ' + pos_1 + ' ' + pos_2
+    elif before_word and not after_word:
+        doc = nlp(before_word + ' ' + word)
+        pos_0 = '<UNKNOWN>' if before_word not in words_freq else doc[0].pos_ + get_tag(doc[0])
+        pos_1 = '<UNKNOWN>' if word not in words_freq else doc[1].pos_ + get_tag(doc[1])
+        context_pos = pos_0 + ' ' + pos_1 + ' __END__'
+    else:
+        doc = nlp(word)
+        pos_1 = '<UNKNOWN>' if word not in words_freq else doc[0].pos_ + get_tag(doc[0])
+        context_pos = '__BEGIN__ ' + pos_1 + ' ' + '__END__'
+    return context_pos
+    
+
+def P(word, original_word, words_freq, pos_freqs, N_pos, before_word, after_word, nlp): 
     "Probability of `word`."
+    context_pos = get_pos_context(before_word, word, after_word, words_freq, nlp)
     if word in words_freq:
-        freq = words_freq[word.lower()][word] / N_pos
+        freq = (words_freq[word.lower()][word] / N_pos) * (60 if word.lower() != original_word.lower() else 100)
     else:
         freq = 0
     if context_pos:
-        pos_freq = pos_freqs[context_pos] / sum(pos_freqs.values())
-        freq *= pos_freq
+        pos_freq = (99 * pos_freqs.get(context_pos, 0) + 1) / sum(pos_freqs.values())
+        freq = freq * pos_freq * 100
     return freq
 
-def correction(word, words_freq, pos_freqs, N_pos, context_pos): 
+def correction(word, words_freq, pos_freqs, N_pos, before_word, after_word, nlp): 
     "Most probable spelling correction for word."
-    return max(candidates(word, words_freq), key=lambda x: P(x, words_freq, pos_freqs, N_pos, context_pos))
+    return max(candidates(word, words_freq), key=lambda x: P(x, word, words_freq, pos_freqs, N_pos, before_word, after_word, nlp))
 
 def candidates(word, words_freq): 
     "Generate possible spelling corrections for word."
@@ -61,35 +99,34 @@ def edits2(word):
     "All edits that are two edits away from `word`."
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
-def correct_line(line, pos_freqs, words_freq):
-    # TODO: Pré-processar: espaços
-    # TODO: Pós-processar: tracinhos
-    nlp = spacy.load('pt')
-    doc = nlp(line)
-    words =  re.findall(r'\w+|\W+', line, flags=re.UNICODE)
+def next_word_on_sentence(current_word, next_words, exceptions):
+    for i, word in enumerate(next_words):
+        if not regex.search(r'\s+', word):
+            return word
+    return None
+
+def correct_line(line, pos_freqs, words_freq, nlp=spacy.load('pt')):
+    words =  regex.findall(r'\w+|\s+|\p{P}+', line, flags=regex.UNICODE)
     new_line = ''
-    i = 0
-    n = len(doc)
     flatten = lambda l: [item for sublist in l for item in sublist]
     N_pos = sum(flatten(map(lambda x: x.values() , words_freq.values())))
-    for word in words:
-        if all([c.isspace() or c in string.punctuation for c in word]):
-            new_line += re.sub(r' +', ' ', word)
+    last_word = None
+    exceptions = ['sr','sra','sras','dr','dra','prof']
+    for i, word in enumerate(words):
+        if word.isspace():
+            new_line += regex.sub(r' +', ' ', word)
+        elif regex.search(r'\p{P}+', word, flags=regex.UNICODE):
+            new_line += word
         else:
             # candidates = candidates(word, words_freq)
-            if i == 0:
-                context_pos = '__BEGIN__ ' + doc[0].pos_ + ' ' + doc[1].pos_
-            elif i!=n-1:
-                context_pos = doc[i-1].pos_ + ' ' + doc[i].pos_ + ' ' + doc[i+1].pos_
-            else:
-                context_pos = doc[i-1].pos_ + ' ' + doc[i].pos_ + ' __END__'
-            new_line += correction(word.lower(), words_freq, pos_freqs, N_pos, context_pos)
-            i += 1
-    exceptions = ['sr','sra','sras','dr','dra','prof']
+            before_word = last_word
+            after_word = next_word_on_sentence(word, words[i+1:], exceptions)
+            last_word = correction(word.lower(), words_freq, pos_freqs, N_pos, before_word, after_word, nlp)
+            new_line += last_word
     # Caracter maiúsculo no início da linha
-    new_line = re.sub(r'^\s*\w', lambda x: x[0].upper(), new_line)
+    new_line = regex.sub(r'^\s*\w', lambda x: x[0].upper(), new_line)
     # Caracter maiúsculo após sinal de pontuação
-    new_line = re.sub(r'(?<=[^(?:' + r'|'.join(exceptions) + r')][\.!?])\s*\w', lambda x: x[0].upper(), new_line)
+    new_line = regex.sub(r'(?<=[^(?:' + r'|'.join(exceptions) + r')][\.!?])\s*\w', lambda x: x[0].upper(), new_line)
     return new_line
 
 
@@ -98,28 +135,37 @@ def correct_line(line, pos_freqs, words_freq):
 
 def correct_line_aspell(line):
     s = aspell.Speller('lang', 'pt_PT')
-    words = re.split(r'\s+', line)
+    words = regex.findall(r'\w+|\s+|\p{P}+', line, flags=regex.UNICODE)
     new_line = []
     for word in words:
-        if word in string.punctuation:
+        if regex.match(r'\s+|\p{P}+', word):
             new_line.append(word)
         else:
-            new_line.append(s.suggest(word)[0])
-    return ' '.join(new_line)
+            suggestions = s.suggest(word)
+            if len(suggestions) > 0:
+                new_line.append(suggestions[0])
+            else:
+                new_line.append(word)
+    return ''.join(new_line)
 
 
 ####################            HUNSPELL             ####################
 def correct_line_hunspell(line):
     hobj = hunspell.HunSpell('dictionaries/hunspell-pt_PT-20171225/pt_PT.dic', 'dictionaries/hunspell-pt_PT-20171225/pt_PT.aff')
-    words = re.split(r'\s+', line)
+    words = regex.findall(r'\w+|\s+|\p{P}+', line, flags=regex.UNICODE)
     new_line = []
     for word in words:
-        if word in string.punctuation:
+        if regex.match(r'\s+|\p{P}+', word):
+            new_line.append(word)
+        elif hobj.spell(word):
             new_line.append(word)
         else:
-            new_line.append(hobj.suggest(word)[0])
-    return ' '.join(new_line)
-
+            suggestions = hobj.suggest(word)
+            if len(suggestions) > 0:
+                new_line.append(suggestions[0])
+            else:
+                new_line.append(word)
+    return ''.join(new_line)
 
 
 ####################            SYMSPELLY            ####################
@@ -135,21 +181,46 @@ def correct_line_symspelly(line):
     term_index = 0  # column of the term in the dictionary text file
     count_index = 1  # column of the term frequency in the dictionary text file
     if not sym_spell.load_dictionary(dictionary_path, term_index, count_index):
-        print("Dictionary file not found")
+        print("ERROR: Dictionary file not found")
         return
 
     # lookup suggestions for single-word input strings
-    # input_term = "memebers"
-    # max_edit_distance_lookup = 2
-    # suggestion_verbosity = Verbosity.CLOSEST  # TOP, CLOSEST, ALL
-    # suggestions = sym_spell.lookup(input_term, suggestion_verbosity, max_edit_distance_lookup)
-    # for suggestion in suggestions:
-    #     print("{}, {}, {}".format(suggestion.term, suggestion.count, suggestion.distance))
+    max_edit_distance_lookup = 2
+    suggestion_verbosity = Verbosity.CLOSEST  # TOP, CLOSEST, ALL
+
+    words = regex.findall(r'\w+|\s+|\p{P}+', line, flags=regex.UNICODE)
+    new_line = []
+    for word in words:
+        if regex.match(r'\s+|\p{P}+', word):
+            new_line.append(word)
+        else:
+            suggestions = sym_spell.lookup(word, suggestion_verbosity, max_edit_distance_lookup)
+            if len(suggestions) > 0:
+                new_line.append(suggestions[0].term)
+            else:
+                new_line.append(word)
+    new_line = ''.join(new_line)
+
 
     # lookup suggestions for multi-word input strings (supports compound splitting & merging)
-    max_edit_distance_lookup = 2
-    suggestions = sym_spell.lookup_compound(line, max_edit_distance_lookup)
-    return ' | '.join([ suggestion.term for suggestion in suggestions ])
+    # tokens = regex.findall(r'[\p{P}\s]+|[\w\s]+', line, flags=regex.UNICODE)
+    # max_edit_distance_lookup = 2
+    # new_line = ''
+    # for token in tokens:
+    #     if regex.match(r'[\p{P}\s]+', token, flags=regex.UNICODE):
+    #         new_line += token
+    #     else:
+    #         suggestions = sym_spell.lookup_compound(token, max_edit_distance_lookup)
+    #         new_line += suggestions[0].term
+    
+    # Caracter maiúsculo no início da linha
+    new_line = regex.sub(r'^\s*\w', lambda x: x[0].upper(), new_line)
+    # Caracter maiúsculo após sinal de pontuação
+    exceptions = ['sr','sra','sras','dr','dra','prof']
+    new_line = regex.sub(r'(?<=[^(?:' + r'|'.join(exceptions) + r')][\.!?])\s*\w', lambda x: x[0].upper(), new_line)
+    return new_line
+
+    return new_line
     # suggestions = sym_spell.word_segmentation(line)
     # return suggestions.corrected_string
 
@@ -167,10 +238,10 @@ def get_pos_frequences(text_fd, dict_words):
     nlp = spacy.load('pt')
     c = Counter()
     words = defaultdict(Counter)
-    # lines = text_fd.readlines()
-    # n_lines = len(lines)
-    # for n_line, line in enumerate(lines):
-    for line in text_fd:
+    lines = text_fd.readlines()
+    n_lines = len(lines)
+    for n_line, line in enumerate(lines):
+    #for line in text_fd:
         doc = nlp(line)
         added_words = []
         for word_doc in doc:
@@ -184,14 +255,17 @@ def get_pos_frequences(text_fd, dict_words):
                 words[word.lower()][word_first_lower] += 1
                 added_words.append(word_doc)
         len_doc = len(doc)
-        if doc[0] in added_words and doc[1] in added_words:
-            c['__BEGIN__ ' + doc[0].pos_ + ' ' + doc[1].pos_] += 1
         for i in range(1,len_doc-1):
-            if doc[i-1] in added_words and doc[i] in added_words and doc[i+1] in added_words: 
-                c[doc[i-1].pos_ + ' ' + doc[i].pos_ + ' ' + doc[i+1].pos_] += 1
-        if doc[len_doc-2] in added_words and doc[len_doc-1] in added_words:
-            c[doc[len_doc-2].pos_ + ' ' + doc[len_doc-1].pos_ + ' __END__'] += 1
-        #print(f'Processado {str(n_line)}/{str(n_lines)}', end='\r')
+            doc_0 = doc[i-1].pos_ + get_tag(doc[i-1]) if doc[i-1] in added_words or doc[i-1].pos_=='PUNCT' else '<UNKNOWN>'
+            doc_1 = doc[i].pos_ + get_tag(doc[i]) if doc[i] in added_words or doc[i].pos_=='PUNCT' else '<UNKNOWN>'
+            doc_2 = doc[i+1].pos_ + get_tag(doc[i+1]) if doc[i+1] in added_words or doc[i+1].pos_=='PUNCT' else '<UNKNOWN>'
+            if doc_1 != '<UNKNOWN>':
+                c[doc_0 + ' ' + doc_1 + ' ' + doc_2] += 1
+                if doc_0 != '<UNKNOWN>' and doc_2 != '<UNKNOWN>':
+                    c['<UNKNOWN>' + ' ' + doc_1 + ' ' + doc_2] += 1
+                    c[doc_0 + ' ' + doc_1 + ' ' + '<UNKNOWN>'] += 1
+        
+        print(f'Processado {str(n_line)}/{str(n_lines)}', end='\r', file=sys.stderr)
     # Adiciona uma entrada para cada palavra do dicionário
     for word in dict_words:
         words[word.lower()][word] += 1
@@ -200,7 +274,7 @@ def get_pos_frequences(text_fd, dict_words):
 
 def analyze_large_text():
     # cat CETEMPublico1.7_100MB | perl -pe 's/\s/ /g' | perl -pe 's/\s*<[^>]+>\s*/\n/g' | perl -pe 's/^\s*$//g' | perl -pe 's/ ([,.!?$%;:])/\1/g' | perl -pe 's/(\(|\[|\{|«|<) /\1/g' | perl -pe 's/ (\)|\]|\}|>|»)/\1/g' > CETEMPublico.txt
-    if not os.path.isfile('dict_info.json'):
+    if not os.path.isfile('dict_info_tags.json'):
         pos_freq = None
         words_freq = None
         with open('dictionaries/wordlist-ao-latest.txt') as dict_fd:
@@ -208,35 +282,83 @@ def analyze_large_text():
             dict_words.sort()
             with open('CETEMPublico_20MB.txt') as text_fd:
                 pos_freq, words_freq = get_pos_frequences(text_fd, dict_words)
-        with open('dict_info.json', 'w' ) as fd:
+        with open('dict_info_tags.json', 'w' ) as fd:
             json.dump((pos_freq, words_freq), fd)
     else:
-        with open('dict_info.json', 'r' ) as fd:
+        with open('dict_info_tags.json', 'r' ) as fd:
             (pos_freq, words_freq) = json.load(fd)
     return pos_freq, words_freq
 
 
 ####################              MAIN               ####################
-def main():
-    # opts, args = getopt.getopt(sys.argv[1:], "bhv", ["build", "help", "version"])
-    # opts_dict = dict(opts)
-
-    print('--Geração de Estatísticas Iniciada--', file=sys.stderr)
-    t1 = time.time()
-    pos_freq, words_freq = analyze_large_text()
-    t2 = time.time()
-    print('--Geração de Estatísticas Terminada--', file=sys.stderr)
-    print('\n--Processamento de texto Iniciado--', file=sys.stderr)
+def correct_text_files(args, pos_freq, words_freq, correct_function):
     new_text = []
-    for line in fileinput.input():
-        new_text.append(correct_line(line.strip(), pos_freq, words_freq))
-    t3 = time.time()
-    print('\n'.join(new_text))
-    print('--Processamento de texto Terminado--', file=sys.stderr)
+    nlp = spacy.load('pt')
+    for line in fileinput.input(args):
+        new_text.append(correct_function(line.strip(), pos_freq, words_freq, nlp))
+    return '\n'.join(new_text)
 
-    print(file=sys.stderr)
-    print('Time Load:\t %.2f sec' % (t2-t1), file=sys.stderr)
-    print('Time Correct:\t %.2f sec' % (t3-t2), file=sys.stderr)
+
+def correct_text_files_ext(args, correct_function):
+    new_text = []
+    for line in fileinput.input(args):
+        new_text.append(correct_function(line.strip()))
+    return '\n'.join(new_text)
+
+
+def correct_text(pos_freq, words_freq, text_lines):
+    new_text = []
+    nlp = spacy.load('pt')
+    for line in text_lines:
+        new_text.append(correct_line(line.strip(), pos_freq, words_freq, nlp))
+    return '\n'.join(new_text)
+
+
+def correct_text_ext(text_lines, correct_function):
+    new_text = []
+    for line in text_lines:
+        new_text.append(correct_function(line.strip()))
+    return '\n'.join(new_text)
+
+
+def main():
+    opts, args = getopt.getopt(sys.argv[1:], "bhvf", ["build", "help", "version", "function="])
+    
+    correct_function = correct_line
+    for o, a in opts:
+        if o in ('-f','--function'):
+            if a == 'aspell':
+                correct_function = correct_line_aspell
+            elif a == 'hunspell':
+                correct_function = correct_line_hunspell
+            elif a == 'symspelly':
+                correct_function = correct_line_symspelly
+            else:
+                print(f'ERROR: Nome de função ({a}) de correção desconhecido', file=sys.stderr)
+                exit(1)
+
+    if correct_function == correct_line:
+        print('--Geração de Estatísticas Iniciada--', file=sys.stderr)
+        t1 = time.time()
+        pos_freq, words_freq = analyze_large_text()
+        t2 = time.time()
+        print('--Geração de Estatísticas Terminada--', file=sys.stderr)
+        print('\n--Processamento de texto Iniciado--', file=sys.stderr)
+        new_text = correct_text_files(args, pos_freq, words_freq, correct_function)
+        t3 = time.time()
+        print(new_text)
+        print('--Processamento de texto Terminado--', file=sys.stderr)
+
+        print(file=sys.stderr)
+        print('Time Load:\t %.2f sec' % (t2-t1), file=sys.stderr)
+        print('Time Correct:\t %.2f sec' % (t3-t2), file=sys.stderr)
+    
+    else:
+        t1 = time.time()
+        new_text = correct_text_files_ext(args, correct_function)
+        t2 = time.time()
+        print(new_text)
+        print('Time Correct:\t %.2f sec' % (t2-t1), file=sys.stderr)
 
 
 if __name__ == "__main__":
